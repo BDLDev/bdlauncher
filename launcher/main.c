8ae8da21f3fb8f051c280167673be56b22d41605
codeHz <codehz@outlook.com>
2020-01-12 01:12:57 +0800

    fix LD_LIBRARY_PATH

--- launcher/main.c
+++ launcher/main.c
@@ -5,11 +5,15 @@
 int main(int argc, char *argv[]) {
     char const *bedrock_server = "./bedrock_server";
     if (argc >= 2) bedrock_server = argv[1];
-    char buf[256];
-    readlink("/proc/self/exe", buf, sizeof buf);
-    *strrchr(buf, '/') = 0;
-    chdir(buf);
-    setenv("LD_LIBRARY_PATH", ".", 1);
+    char pathbuf[256], ldbuf[1024] = ".";
+    readlink("/proc/self/exe", pathbuf, sizeof pathbuf);
+    *strrchr(pathbuf, '/') = 0;
+    chdir(pathbuf);
+    if (getenv("LD_LIBRARY_PATH")) {
+        strcat(ldbuf, ":");
+        strcat(ldbuf, getenv("LD_LIBRARY_PATH"));
+    }
+    setenv("LD_LIBRARY_PATH", ldbuf, 1);
     setenv("LD_PRELOAD", "preload.so", 1);
     return execv(bedrock_server, argv);
 
