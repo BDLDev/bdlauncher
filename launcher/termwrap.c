ff7d9a8aca35638828383fe52e106672e16affe6
CodeHz <CodeHz@outlook.com>
2020-01-13 15:04:28 +0800

    improve output wrap

--- launcher/termwrap.c
+++ launcher/termwrap.c
@@ -11,6 +11,7 @@
 #include <sys/signalfd.h>
 #include <sys/wait.h>
 #include <readline/readline.h>
+#include <readline/history.h>
 
 #include <copoll.h>
 
@@ -25,6 +26,7 @@ static copoll_co_ref_t readline_co = NULL, output_co = NULL;
 
 void handle_line(char *line) {
   if (line) {
+    add_history(line);
     fprintf(ptyfile, "%s\n", line);
   } else
     printf("\r");
@@ -38,7 +40,46 @@ void wrap_output(char *data, ssize_t length) {
   rl_set_prompt("");
   rl_replace_line("", 0);
   rl_redisplay();
-  printf("%.*s", (int) length, data);
+  while (length--) {
+    char unsigned ch = (char unsigned) *data++;
+    switch (ch) {
+    case '\n': puts("\e[0m"); break;
+    case 0xc2: {
+      if (!length-- || !length--) continue;
+      ++data;
+      switch (*data++) {
+#define PUTCOL(c, color, code)                                                                                         \
+  case c: fputs("\e[" code "m", stdout); break
+        PUTCOL('0', #000000, "30;107");
+        PUTCOL('1', #0000AA, "38;2;0;0;170");
+        PUTCOL('2', #00AA00, "38;2;0;170;0");
+        PUTCOL('3', #00AAAA, "38;2;0;170;170");
+        PUTCOL('4', #AA0000, "38;2;170;0;0");
+        PUTCOL('5', #AA00AA, "38;2;170;0;170");
+        PUTCOL('6', #FFAA00, "38;2;255;170;0");
+        PUTCOL('7', #AAAAAA, "38;2;170;170;170");
+        PUTCOL('8', #555555, "38;2;85;85;85");
+        PUTCOL('9', #5555FF, "38;2;85;85;255");
+        PUTCOL('a', #55FF55, "38;2;85;255;85");
+        PUTCOL('b', #55FFFF, "38;2;85;255;255");
+        PUTCOL('c', #FF5555, "38;2;255;85;85");
+        PUTCOL('d', #FF55FF, "38;2;255;85;255");
+        PUTCOL('e', #FFFF55, "38;2;255;255;85");
+        PUTCOL('f', #FFFFFF, "97;40");
+        // Special effect
+        PUTCOL('k', Obfuscated, "5");
+        PUTCOL('l', Bold, "1");
+        PUTCOL('m', Strikethrough, "9");
+        PUTCOL('n', Underline, "4");
+        PUTCOL('o', Italic, "3");
+        PUTCOL('r', Reset, "0");
+#undef PUTCOL
+      default: break;
+      }
+    } break;
+    default: putc(ch, stdout);
+    }
+  }
   rl_set_prompt(prompt);
   rl_replace_line(saved_line, 0);
   rl_point = saved_point;
@@ -119,13 +160,16 @@ int termwrap() {
     tcsetattr(ptyfd, TCSANOW, &t);
     ptyfile = fdopen(ptyfd, "w");
     rl_callback_handler_install(prompt, handle_line);
+    rl_bind_key('\t', rl_insert);
+    read_history(".bdlauncher_history");
 
     copoll_ctx_ref_t ctx = copoll_init();
     copoll_start(ctx, get_input, NULL, 1024 * 8);
     copoll_start(ctx, get_output, NULL, 1024 * 8);
     copoll_start(ctx, sig_handler, NULL, 1024 * 8);
     copoll_fini(ctx);
-    printf("done!\n");
+    printf("\rdone!\n");
+    write_history(".bdlauncher_history");
     rl_callback_handler_remove();
   }
   return -1;
