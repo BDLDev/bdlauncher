b072e7ad32c23724ccf78fda79b53c562f1107fa
CodeHz <CodeHz@outlook.com>
2020-01-13 20:53:07 +0800

    tweak jmpbed

--- preload/hook.cpp
+++ preload/hook.cpp
@@ -1,18 +1,55 @@
 #include "myhook.h"
 #include "hook.h"
-static char *jmpbed;
+#include <global.h>
+#include <type_traits>
+
+template <size_t size> struct StrictMem {
+  char *current;
+
+  INLINE StrictMem(char *ptr) { current = ptr; }
+  template <size_t use> INLINE auto operator<<(char const (&data)[use]) -> StrictMem<size - use> {
+    static_assert(size >= use);
+    memcpy(current, data, use);
+    return {current + use};
+  }
+  template <typename T>
+  INLINE auto operator<<(T addr) -> std::enable_if_t<std::is_same_v<T, void *>, StrictMem<size - 8>> {
+    static_assert(size >= 8);
+    memcpy(current, &addr, 8);
+    return {current + 8};
+  }
+  INLINE operator char *() { return current; }
+};
+
+template <size_t base> class AutoGrowMem {
+  char *current, *top;
+
+  INLINE void realloc() {
+    current =
+        static_cast<char *>(mmap(0, base, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+    top = current + base;
+  }
+
+public:
+  INLINE AutoGrowMem() { realloc(); }
+
+  template <size_t use> INLINE StrictMem<use> alloc() {
+    static_assert(use < base);
+    if (current + use > top) realloc();
+    auto tmp = current;
+    current += use;
+    return {tmp};
+  }
+};
+
+static AutoGrowMem<65536> jmpbed;
 // extern std::unordered_map<void*,char*> my_hooks;
 char *wr_jmp(void *addr) {
-  if (jmpbed == nullptr) jmpbed = (char *) mmap(0, 65536, 7, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  char *ret = jmpbed;
-  memcpy(jmpbed, "\xff\x25\x00\x00\x00\x00", 6);
-  memcpy(jmpbed + 6, &addr, 8);
-  jmpbed += 14;
-  return ret;
+  auto mem = jmpbed.alloc<16>();
+  mem << "\xff\x25\x00\x00\x00\x00" << addr;
+  return mem;
 }
 char *wr_regcmd(void *call, void *arg) {
-  if (jmpbed == nullptr) jmpbed = (char *) mmap(0, 65536, 7, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  char *ret = jmpbed;
   /*
      0x0000000000001190 <+0>:	57	push   rdi
  0x0000000000001191 <+1>:	48 b8 77 77 77 77 ff ff ff ff	movabs rax,0xffffffff77777777
@@ -23,22 +60,11 @@ char *wr_regcmd(void *call, void *arg) {
  0x00000000000011ab <+27>:	48 89 f8	mov    rax,rdi
  0x00000000000011ae <+30>:	c3	ret
  */
-  memcpy(jmpbed, "\x57\x48\xb8", 3);
-  jmpbed += 3;
-  memcpy(jmpbed, &call, 8);
-  jmpbed += 8;
-  memcpy(jmpbed, "\x48\xbf", 2);
-  jmpbed += 2;
-  memcpy(jmpbed, &arg, 8);
-  jmpbed += 8;
-  memcpy(jmpbed, "\xff\xd0\x5f\x48\x89\x07\x48\x89\xf8\xc3", 10);
-  jmpbed += 10;
-  return ret;
+  auto mem = jmpbed.alloc<48>();
+  mem << "\x57\x48\xb8" << call << "\x48\xbf" << arg << "\xff\xd0\x5f\x48\x89\x07\x48\x89\xf8\xc3";
+  return mem;
 }
 void MyPatch(void *oldfunc, void *newfunc, size_t size) {
-#define PAGE_SIZE 0x1000
-#define ROUND_PAGE_DOWN(num) ((num) & ~(PAGE_SIZE - 1))
-#define ROUND_PAGE_UP(num) (((num) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
   auto start = ROUND_PAGE_DOWN((size_t) oldfunc);
   auto end   = ROUND_PAGE_UP((size_t) oldfunc + size);
   mprotect((void *) start, end - start, PROT_READ | PROT_WRITE | PROT_EXEC);
