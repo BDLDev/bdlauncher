6c759a4cc1d932fe2555177cd0b71d369c1332c9
Sysca11 <sysca11@github.com>
2020-02-04 19:01:00 +0800

    better makefile,fix mcbug#1

--- include/minecraft/core/vtSP.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-extern "C" {
-void _ZTV12ServerPlayer(void); // dummy
-}
-
-// utils for runtime type detecting
-static auto vtSP = ((void **) _ZTV12ServerPlayer) + 2; //((void **)dlsym(NULL, "_ZTV12ServerPlayer") + 2);
